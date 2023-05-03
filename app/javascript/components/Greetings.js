f75452b5efc6812b333a0fb0021e54a18eb54830
codehass <hassan.elouardy06@gmail.com>
2023-05-03 22:42:17 +0000

    Add necessary code to greetings component

--- app/javascript/components/Greetings.js
+++ app/javascript/components/Greetings.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGreeting } from '../store/greetings/greetings';
 
-function Greetings() {
-  return <h1>Hello</h1>;
-}
+const Greeting = () => {
+  const dispatch = useDispatch();
+  const greeting = useSelector((state) => state.greetings.greeting);
 
-export default Greetings;
+  useEffect(() => {
+    dispatch(fetchGreeting());
+  }, []);
+
+  return (
+    <>
+      <h1>Greeting App </h1>
+      <h3>{greeting}</h3>
+    </>
+  );
+};
+
+export default Greeting;
