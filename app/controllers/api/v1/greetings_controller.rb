ef5f1633e521c3ae7219145c6f258183fb0b8dfa
codehass <hassan.elouardy06@gmail.com>
2023-05-03 22:51:03 +0000

    Fix linters

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class GreetingsController < ApplicationController
       def index
-        greeting = Greeting.order("RANDOM()").first
+        greeting = Greeting.order('RANDOM()').first
         render json: { greeting: greeting.name }
       end
     end
